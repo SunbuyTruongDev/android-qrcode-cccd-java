50f72a6fb5815d231eb0566280e607ddcd5f847d
Xuantruong21 <Xuantruongcarbon@gmail.com>
2023-01-07 11:12:25 +0700

    remove module kotlin

--- scancode/src/main/java/com/sunbuy/scancode/MlKitErrorHandler.kt
+++
@@ -1,9 +0,0 @@
-package com.sunbuy.scancode
-
-import android.app.Activity
-
-object MlKitErrorHandler {
-
-  @Suppress("UNUSED_PARAMETER", "FunctionOnlyReturningConstant")
-  fun isResolvableError(activity: Activity, exception: Exception) = false // always false when bundled
-}
